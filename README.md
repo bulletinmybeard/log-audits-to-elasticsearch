5a415c1de7bbeb225e4657963a9fc3f36b2d8212
Robin Schulz <hello@rschu.me>
2024-03-23 19:16:04 +0100

    - Python version update script created to update the application version in multiple different files from the file paths defined in `file_path_list` - README updated

--- README.md
+++ README.md
@@ -233,6 +233,14 @@ poetry run coverage run -m pytest -s
 poetry run coverage run -m pytest -s -vv
 ```
 
+## Python Update Script
+This script is used to update the application version in multiple files. The script can be called like this:
+```bash
+python update_version.py 0.5.0 0.6.0
+```
+
+```bash
+
 ## TODO
 - [ ] Finalize the README and documentation
 - [ ] Finalize the audit log search filters (`/search`)
---
+++ update_version.py
@@ -0,0 +1,77 @@
+import sys
+import re
+from contextlib import contextmanager
+from typing import List
+
+if len(sys.argv) != 3:
+    print("Usage: python update_version.py <current_version> <new_version>")
+    sys.exit(1)
+
+arg_current_version = str(sys.argv[1])
+arg_new_version = str(sys.argv[2])
+
+semver_regex = r'^\d+\.\d+\.\d+$'
+
+# Check if both versions match the Semantic Versioning pattern.
+if not re.match(semver_regex, arg_current_version):
+    print(f"Error: Current version '{arg_current_version}' does not match the Semantic Versioning pattern.")
+    sys.exit(1)
+
+if not re.match(semver_regex, arg_new_version):
+    print(f"Error: New version '{arg_new_version}' does not match the Semantic Versioning pattern.")
+    sys.exit(1)
+
+# Abort if both versions are identical.
+if arg_current_version == arg_new_version:
+    print(f"Error: New version '{arg_new_version}' equals the current version '{arg_current_version}' (nothing to do here).")
+    sys.exit(1)
+
+# File paths where the version numbers should be updated.
+file_path_list = [
+    'pyproject.toml',
+    'service_audit/__init__.py',
+    'service_audit/main.py',
+]
+
+error_messages = []
+
+
+@contextmanager
+def update_version_in_file(file_path: str, current_version: str, new_version: str) -> None:
+    # Read the file.
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    # Check if the current version is to find in the file.
+    if current_version not in content:
+        error_messages.append(f"Error: current version '{current_version}' not found in '{file_path}'")
+        yield
+        return
+
+    try:
+        # Write the updated content back to the file.
+        with open(file_path, 'w') as file:
+            # Replace the current version with the new version.
+            file.write(content.replace(current_version, new_version))
+    except Exception as e:
+        error_messages.append(f"Error: {e}")
+        yield
+        return
+
+    print(f"Updated version in {file_path} to {new_version}")
+    yield
+
+
+def update_versions(file_paths: List[str], current_version: str, new_version: str) -> None:
+    for file_path in file_paths:
+        with update_version_in_file(file_path, current_version, new_version):
+            pass
+
+    if error_messages:
+        for error in error_messages:
+            print(error)
+        sys.exit(1)
+
+
+# Update the versions.
+update_versions(file_path_list, arg_current_version, arg_new_version)
